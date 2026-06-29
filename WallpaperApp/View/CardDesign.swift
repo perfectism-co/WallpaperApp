fbd4b44583beb7b6b96d442b675109e1ef9b6113
perfectism-co <perfectism.co@gmail.com>
2026-06-29 14:38:40 +0800

    卡片樣式切換功能

---
+++ WallpaperApp/View/CardDesign.swift
@@ -0,0 +1,18 @@
+//
+//  CardDesign.swift
+//  WallpaperApp
+//
+//  Created by macmini on 2026/6/28.
+//
+
+import SwiftUI
+
+struct CardDesign: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardDesign()
+}
